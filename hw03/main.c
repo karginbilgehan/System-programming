9d2a5f97ac7cc91af4de9ad466a12388637373d9
Abdullah Bilgehan Kargın <abilgehankargin@gmail.com>
2019-04-04 16:25:26 +0300

    Update main.c

--- hw03/main.c
+++ hw03/main.c
@@ -17,14 +17,21 @@
 
 int exitCall();
 void cd(char *command[]);
-void findCommand(char ** split_command,char *command);
-void selectCommand(char *command[]);
+void findCommand(char ** split_command,char *command,int* pipe);
+void selectCommand(char *command[],int* pipe);
+void selectCommandForPipe(char* parseCommand[]);
 void help();
+
+int beforePipeCounter=0;
+int afterPipeCounter=0;
+
 int main(int argc, char* argv[]){
 	int exitStat=1;
 	char buffer[COMMAND_SIZE];
 	char historyBuff[1024][COMMAND_SIZE];//it is for !n command.
 	int historyIndex=0;
+	int forPipe=0;
+	
 	while(exitStat){
 		char **split_command = malloc(10 * sizeof(char*));
 		int i = 0;
@@ -38,7 +45,7 @@ int main(int argc, char* argv[]){
 		if(buffer[0]=='!'){//it is for !n command.
 			int index=(int)buffer[1]-48;
 			printf("index: %d \n",index);
-			findCommand(split_command,historyBuff[index]);
+			findCommand(split_command,historyBuff[index],&forPipe);
 			
 		}			
 		
@@ -46,16 +53,20 @@ int main(int argc, char* argv[]){
 			strcpy(historyBuff[historyIndex],buffer);//it is to keep al command in an array.
 			printf("history: %s \n",historyBuff[historyIndex]);
 			historyIndex++;
-			findCommand(split_command,buffer);
+			findCommand(split_command,buffer,&forPipe);
 		}
 		
-		for(i=0;i<historyIndex;++i){
+		/*for(i=0;i<historyIndex;++i){
 			printf("history[%d]: %s \n",i,historyBuff[i]);
-		}
+		}*/
 		//printf("%s\n",split_command[0]);
 		//printf("whole command %s \n",split_command[0]);
-		selectCommand(split_command);
 		
+		selectCommand(split_command,&forPipe);
+		
+		beforePipeCounter=0;
+		afterPipeCounter=0;
+		forPipe=0;
 		
 		for (i = 0; i < 10; i++)
 		{
@@ -67,14 +78,20 @@ int main(int argc, char* argv[]){
 	}
 }
 
-void findCommand(char ** split_command,char *command){
+void findCommand(char ** split_command,char *command,int* pipe){
 	int index=0;
 	int tempIndex=0;
 	int splitIndex=0;
+	
 	int i;
 	char aCommand[COMMAND_SIZE];
 	char tempCommand[COMMAND_SIZE]={0};
+	
+	
 	while(command[index]!='\n'){
+		if(command[index]=='|'){
+			*pipe=1;
+		}
 		if(command[index]!=' '){
 			
 			tempCommand[tempIndex]=command[index];
@@ -90,6 +107,12 @@ void findCommand(char ** split_command,char *command){
 				tempCommand[i]='\0';
 			}
 			tempIndex=0;
+			if(*pipe==0){
+				beforePipeCounter++;
+			}
+			if(*pipe==1){
+				afterPipeCounter++;
+			}
 		}
 		
 		if(command[index+1] == '\n'){
@@ -102,16 +125,18 @@ void findCommand(char ** split_command,char *command){
 				tempCommand[i]='\0';
 			}
 			tempIndex=0;
-
+			//*afterPipeCounter+=1;
 		}					
 		index++;						
 			
-	}
-
-}
-void selectCommand(char *command[]){
+	}	
+		
+}	
+void selectCommand(char *command[],int* pipe){
        pid_t childPid;
        //printf("value %d \n",strcmp(command,"exit\n"));
+
+
        if(strcmp(command[0],"exit")==0){
 		exitCall();
        }
@@ -126,6 +151,7 @@ void selectCommand(char *command[]){
 		if(strcmp(command[1],"-z")!=0){
 			childPid=fork();
 			if(childPid==0){
+				
 				char *args[]={"./buNeDu",command[1]};
 				args[2]=NULL;
 				//printf("args: %s \n",args[2]); 
@@ -153,46 +179,126 @@ void selectCommand(char *command[]){
        }
        
        else if(strcmp(command[0],"pwd")==0){
-		childPid=fork();
-		if(childPid==0){
-			char *args[]={"./pwd",NULL}; 
-			execv(args[0],args);
+		if(*pipe==0){
+			childPid=fork();
+			if(childPid==0){
+				char *args[]={"./pwd",NULL}; 
+				execv(args[0],args);
+			}
+			else{
+				wait(NULL);
+			}
 		}
-		else{
-			wait(NULL);
-		}
+		if(*pipe ==1){
+			selectCommandForPipe(command);
+		}		
+		
        }
 
        else if(strcmp(command[0],"lsf")==0){
-		childPid=fork();
-		if(childPid==0){
-			char *args[]={"./lsf",NULL}; 
-			execv(args[0],args);
+		if(*pipe==0){
+			childPid=fork();
+			if(childPid==0){
+				char *args[]={"./lsf",NULL}; 
+				execv(args[0],args);
+			}
+			else{
+				wait(NULL);
+			}
 		}
-		else{
-			wait(NULL);
-		}
-       }
+		if(*pipe ==1){
+			selectCommandForPipe(command);
+		}		
+		
+	}
        else if(strcmp(command[0],"cat")==0){
-		childPid=fork();
-		if(childPid==0){
-			char *args[]={"./cat",command[1]}; 
-			execv(args[0],args);
+		if(*pipe ==0){
+			childPid=fork();
+			if(childPid==0){
+				char *args[]={"./cat",command[1]}; 
+				execv(args[0],args);
+			}
+			else{
+				wait(NULL);
+			}
 		}
-		else{
-			wait(NULL);
-		}
+		if(*pipe ==1){
+			selectCommandForPipe(command);
+		}		
+		
        } 
        else if(strcmp(command[0],"wc")==0){
-		childPid=fork();
-		if(childPid==0){
-			char *args[]={"./wc",command[1]}; 
-			execv(args[0],args);
-		}
-		else{
-			wait(NULL);
+		if(*pipe==0){
+			childPid=fork();
+			if(childPid==0){
+				char *args[]={"./wc",command[1]}; 
+				execv(args[0],args);
+			}
+			else{
+				wait(NULL);
+			}	
+		}		
+		if(*pipe ==1){
+			selectCommandForPipe(command);
 		}
        }
+}
+void selectCommandForPipe(char* parseCommand[]){
+	printf("afterPipeCounter %d \n",afterPipeCounter);
+	printf("beforePipeCounter %d \n",beforePipeCounter);
+	int pfd[2];
+	if(pipe(pfd)==-1)
+		printf("pipe");
+
+	switch(fork()){
+		case -1:
+			printf("fork");
+		case 0:
+			if(close(pfd[0])==-1)
+				printf("close 1");
+			if(pfd[1] != STDOUT_FILENO){
+				if(dup2(pfd[1], STDOUT_FILENO)==-1)
+					printf("dup2 1");
+				if(close(pfd[1]) == -1)	
+					printf("close 2");		
+			}
+			
+			execlp("ls","ls",(char*) NULL);
+			printf("execlp ls");
+		default:
+			break;
+	}
+
+	switch(fork()){
+		case -1:
+			printf("fork");
+		case 0:
+			if(close(pfd[1])==-1)
+				printf("close 3");
+			if(pfd[0] != STDIN_FILENO){
+				if(dup2(pfd[0], STDIN_FILENO)==-1)
+					printf("dup2 2");
+				if(close(pfd[0]) == -1)	
+					printf("close 4");		
+			}
+			
+			execlp("wc","wc","-l",(char*) NULL);
+			printf("execlp wc");
+		default:
+			break;
+	}
+
+	if(close(pfd[0])==-1)
+		printf("close 5");
+	if(close(pfd[1])==-1)
+		printf("close 6");
+	if(wait(NULL)==-1)
+		printf("wait 1");
+	if(wait(NULL)==-1)
+		printf("wait 2");
+
+	//exit(EXIT_SUCCESS);
+
 }
 
 int exitCall(){
