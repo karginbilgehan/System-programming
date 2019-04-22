e6f615bc0249f82598bb1af48e9310dbfc9e8d03
Abdullah Bilgehan Kargın <abilgehankargin@gmail.com>
2019-04-22 17:50:40 +0300

    Update client.c

--- hw05/client.c
+++ hw05/client.c
@@ -13,73 +13,74 @@ removeFifo(void) {
 int main(int argc, char *argv[]) {
 	int serverFd, clientFd;
 	int i;
+	pid_t p;
 	struct request req;
 	struct response resp;
 	if (argc > 1 && strcmp(argv[1], "--help") == 0) 
 		perror("[seq-len...]\n");
 	/* Create our FIFO (before sending request, to avoid a race) */
-	umask(0); /* So we get the permissions we want */
-	snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
+	//umask(0); /* So we get the permissions we want */
+	/*snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
 	if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1 && errno != EEXIST)
 		perror("mkfifo");
 	if (atexit(removeFifo) != 0)
 		perror("atexit");
 		
 	/* Construct request message, open server FIFO, and send request */
-	
-		
-	for(i=0;i<2;++i)
-	{
-		if(fork()==0){//child process
-			snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
-			if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1 && errno != EEXIST)
-				perror("mkfifo");
-			if (atexit(removeFifo) != 0)
+	int totalProcess=atoi(argv[1]);
+	for (i = 0; i < totalProcess; ++i) {
+		p = fork();
+		if (p == 0)
+			break;
+	}
+	if(p==0){//child process
+		snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
+		if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1 && errno != EEXIST)
+			perror("mkfifo");
+		if (atexit(removeFifo) != 0)
 			perror("atexit");
-			req.pid = getpid();
-			printf("pid child: %d \n",req.pid);
-			//req.seqLen = atoi(argv[1]);
-			serverFd = open(SERVER_FIFO, O_WRONLY);
-			if (serverFd == -1)
-				perror("open");
-			if (write(serverFd, &req, sizeof(struct request)) != sizeof(struct request))
-				perror("Can't write to server");
+		req.pid = getpid();
+		//printf("pid child: %d \n",req.pid);
+		//req.seqLen = atoi(argv[1]);
+		serverFd = open(SERVER_FIFO, O_WRONLY);
+		if (serverFd == -1)
+			perror("open");
+		if (write(serverFd, &req, sizeof(struct request)) != sizeof(struct request))
+			perror("Can't write to server");
 
-			/* Open our FIFO, read and display response */
-			clientFd = open(clientFifo, O_RDONLY);
-			if (clientFd == -1)
-				perror("open");
-			if (read(clientFd, &resp, sizeof(struct response)) != sizeof(struct response))
-				perror("Can't read response from server");
-			printf("%d\n", resp.money);	
-		}	
-		
+		/* Open our FIFO, read and display response */
+		clientFd = open(clientFifo, O_RDONLY);
+		if (clientFd == -1)
+			perror("open");
+		if (read(clientFd, &resp, sizeof(struct response)) != sizeof(struct response))
+			perror("Can't read response from server");
+		printf("%d TL odendi.\n", resp.money);
+		exit(EXIT_SUCCESS);	
 	}	
-	
-	for(i=0;i<2;i++){
+	for(i=0;i<totalProcess;i++){
 		wait(NULL);
 	}
 		
-	snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
+	/*snprintf(clientFifo, CLIENT_FIFO_NAME_LEN, CLIENT_FIFO_TEMPLATE, (long) getpid());
 	if (mkfifo(clientFifo, S_IRUSR | S_IWUSR | S_IWGRP) == -1 && errno != EEXIST)
 		perror("mkfifo");
 	if (atexit(removeFifo) != 0)
-	perror("atexit");
+		perror("atexit");
 	req.pid = getpid();
 	printf("pid parent: %d \n",req.pid);
-	//req.seqLen = atoi(argv[1]);
-	serverFd = open(SERVER_FIFO, O_WRONLY);
+	//req.seqLen = atoi(argv[1]);*/
+	/*serverFd = open(SERVER_FIFO, O_WRONLY);
 	if (serverFd == -1)
 		perror("open");
 	if (write(serverFd, &req, sizeof(struct request)) != sizeof(struct request))
 		perror("Can't write to server");
 	/* Open our FIFO, read and display response */
-	clientFd = open(clientFifo, O_RDONLY);
+	/*clientFd = open(clientFifo, O_RDONLY);
 	if (clientFd == -1)
 		perror("open");
 	if (read(clientFd, &resp, sizeof(struct response)) != sizeof(struct response))
 		perror("Can't read response from server");
-	printf("%d\n", resp.money);
+	printf("%d\n", resp.money);*/
 	exit(EXIT_SUCCESS);
 
 	
