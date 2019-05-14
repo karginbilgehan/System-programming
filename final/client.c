12f6853bcd9c0fb9fc796c36a8b9a8d43e216d03
Abdullah Bilgehan Kargın <abilgehankargin@gmail.com>
2019-05-14 11:22:50 +0300

    Update client.c

--- final/client.c
+++ final/client.c
@@ -1,1 +1,57 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <fcntl.h> // for open
+#include <unistd.h> // for close
 
+
+int main(int argc, char* argv[]){
+
+  //printf("In thread\n");
+  //printf("client deneme2 \n");
+  char dirName[256];//gönderilecek olan directorynin ismi
+  char buffer[1024];
+  int clientSocket;
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+  // Create the socket. 
+  
+    clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+    
+  //Configure settings of the server address
+ // Address family is Internet 
+    serverAddr.sin_family = AF_INET;
+  //Set port number, using htons function 
+    serverAddr.sin_port = htons(8080);
+ //Set IP address to localhost
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    //Connect the socket to the server using the address
+    addr_size = sizeof serverAddr;
+   
+    if(connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size)==0)
+		printf("connected \n");
+    //printf("client deneme \n");
+    sprintf(dirName,"%s",argv[1]);
+    //printf("client deneme 7 \n");
+    if( send(clientSocket , dirName , strlen(dirName) , 0) < 0)
+    {
+            printf("Send failed\n");
+    }
+    //printf("client deneme 8 \n");
+    //Read the message from the server into the buffer
+    /*if(recv(clientSocket, buffer, 1024, 0) < 0)
+    {
+       printf("Receive failed\n");
+    }*/
+    
+    //Print the received message
+    //printf("Data received: %s\n",buffer);
+    close(clientSocket);
+   
+ 
+  return 0;
+}
