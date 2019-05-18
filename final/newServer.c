22a32f17d9966c2b250805e83d3f7702d2772cd4
Abdullah Bilgehan Kargın <abilgehankargin@gmail.com>
2019-05-18 13:14:56 +0300

    Update newServer.c

--- final/newServer.c
+++ final/newServer.c
@@ -44,10 +44,77 @@ fileInf fileInformation;//client tarafından gelen file bilgilerini almak için
 
 void parseDirectoryName(char* fullPath);
 void receive();
+void * socketThread(void *arg);
 
-void * socketThread(void *arg)
-{
+
+
+
+int main(int argc, char* argv[]){
+  int serverSocket;
+  mainSocket sock;
+  struct sockaddr_in serverAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size;
+
   
+  //fileInformation.file_counter=1;
+  sprintf(sock.serverName,"%s",argv[1]); // BU KISIM DEGİSMELİ 
+  printf("serverName: %s \n",sock.serverName);
+  printf("Server IP Address: 127.0.0.1 \n");
+  //if(chdir(sock.serverName)<0)
+	//printf("Current Directory can not change successfully \n"); // server klasörü altında gerekli işlemleri yapmak için
+  
+  //Create the socket. 
+  serverSocket = socket(PF_INET, SOCK_STREAM, 0);
+  // Configure settings of the server address struct
+  // Address family = Internet 
+  serverAddr.sin_family = AF_INET;
+  //Set port number, using htons function to use proper byte order 
+  serverAddr.sin_port = htons(atoi(argv[3]));
+  //Set IP address to localhost 
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  //Set all bits of the padding field to 0 
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+  //Bind the address struct to the socket 
+  if(bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr))==0)
+	printf("binded \n");
+  //Listen on the socket, with 40 max connection requests queued 
+  if(listen(serverSocket,50)==0)
+    printf("Listening\n");
+  else
+    printf("Error\n");
+    int threadSize=atoi(argv[2]);
+    pthread_t tid[threadSize];
+    int i = 0;
+    //printf("deneme \n");
+    while(1)
+    {
+	//printf("deneme2 \n");
+        //Accept call creates a new socket for the incoming connection
+        addr_size = sizeof serverStorage;
+        sock.newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
+	sprintf(sock.serverName,"%s",argv[1]);
+        //for each client request creates a thread and assign the client request to it to process
+       //so the main thread can entertain next request
+	//printf("deneme3 \n");
+        if( pthread_create(&tid[i], NULL, socketThread, &sock) != 0 )
+           printf("Failed to create thread\n");
+	//printf("deneme4 \n");
+        if( i >= threadSize)
+        {
+          i = 0;
+          while(i<threadSize)
+          {
+            pthread_join(tid[i++],NULL);
+          }
+          i = 0;
+        }
+    }
+  return 0;
+}
+
+void * socketThread(void *arg){
+ 
   //printf("girdim \n");
   mainSocket new = *((mainSocket *)arg);
   char synch[5]="synch";
@@ -57,11 +124,11 @@ void * socketThread(void *arg)
   char curDirPath[PATH_LEN];//kopyalama yapılacak directoryi tutmak için
   char realclientDirName[PATH_LEN];
   struct dirent *currentFile;
-  
-  recv(new.newSocket , clientDirectory , sizeof(clientDirectory) , 0);
-  send(new.newSocket,synch,sizeof(synch),0);
-  // Send message to the client socket 
   pthread_mutex_lock(&lock);
+  recv(new.newSocket , clientDirectory , sizeof(clientDirectory) , 0);
+  //send(new.newSocket,synch,sizeof(synch),0);
+  // Send message to the client socket 
+  
   char *message = malloc(sizeof(clientDirectory)+20);
   strcpy(message,"Client Directory Path: ");
   strcat(message,clientDirectory);
@@ -76,7 +143,7 @@ void * socketThread(void *arg)
   parseDirectoryName(buffer);
   
   sprintf(realclientDirName,"%s/%s",new.serverName,clientDirName);
-  printf("client directorty: %s \n",realclientDirName);
+  printf("client directory: %s \n",realclientDirName);
   directory=opendir(realclientDirName);
   if(directory==NULL){
 	if(mkdir(realclientDirName, S_IRUSR | S_IWUSR | S_IXUSR)==-1)
@@ -100,72 +167,8 @@ void * socketThread(void *arg)
   }
   pthread_mutex_unlock(&lock);
   close(new.newSocket);
-  printf("Exit socketThread \n");
+  printf("End of the thread \n");
   pthread_exit(NULL);
-}
-
-
-
-int main(int argc, char* argv[]){
-  int serverSocket;
-  mainSocket sock;
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
-
-
-  //fileInformation.file_counter=1;
-  sprintf(sock.serverName,"%s",argv[1]); // BU KISIM DEGİSMELİ 
-  printf("serverName: %s \n",sock.serverName);
-  //if(chdir(sock.serverName)<0)
-	//printf("Current Directory can not change successfully \n"); // server klasörü altında gerekli işlemleri yapmak için
-  
-  //Create the socket. 
-  serverSocket = socket(PF_INET, SOCK_STREAM, 0);
-  // Configure settings of the server address struct
-  // Address family = Internet 
-  serverAddr.sin_family = AF_INET;
-  //Set port number, using htons function to use proper byte order 
-  serverAddr.sin_port = htons(8080);
-  //Set IP address to localhost 
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  //Set all bits of the padding field to 0 
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-  //Bind the address struct to the socket 
-  if(bind(serverSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr))==0)
-	printf("binded \n");
-  //Listen on the socket, with 40 max connection requests queued 
-  if(listen(serverSocket,50)==0)
-    printf("Listening\n");
-  else
-    printf("Error\n");
-    pthread_t tid[60];
-    int i = 0;
-    //printf("deneme \n");
-    while(1)
-    {
-	//printf("deneme2 \n");
-        //Accept call creates a new socket for the incoming connection
-        addr_size = sizeof serverStorage;
-        sock.newSocket = accept(serverSocket, (struct sockaddr *) &serverStorage, &addr_size);
-	sprintf(sock.serverName,"%s",argv[1]);
-        //for each client request creates a thread and assign the client request to it to process
-       //so the main thread can entertain next request
-	//printf("deneme3 \n");
-        if( pthread_create(&tid[i], NULL, socketThread, &sock) != 0 )
-           printf("Failed to create thread\n");
-	//printf("deneme4 \n");
-        if( i >= 50)
-        {
-          i = 0;
-          while(i < 50)
-          {
-            pthread_join(tid[i++],NULL);
-          }
-          i = 0;
-        }
-    }
-  return 0;
 }
 
 void parseDirectoryName(char* fullPath){
@@ -187,7 +190,7 @@ void parseDirectoryName(char* fullPath){
 	
 }
 void receive(mainSocket new,char *realPath){
-	  printf("geldim \n"); 
+	  //printf("geldim \n"); 
           int outFd;
 	  char copiedFile[PATH_LEN];
 	  char copiedFilePath[PATH_LEN];
@@ -196,27 +199,27 @@ void receive(mainSocket new,char *realPath){
 	  char synch[5]="synch";
  	  DIR *directory;
 	  while(j<20){
-		printf("geldim \n"); 
+		//printf("geldim \n"); 
 		recv(new.newSocket,&fileInformation.rec_mode,sizeof(fileInformation.rec_mode),0);
-		send(new.newSocket,synch,sizeof(synch),0);
+		//send(new.newSocket,synch,sizeof(synch),0);
 		if(fileInformation.rec_mode==1){
 			break;
 		}
 	  	recv(new.newSocket , fileInformation.filename , sizeof(fileInformation.filename) , 0);
-		send(new.newSocket,synch,sizeof(synch),0);
+		//send(new.newSocket,synch,sizeof(synch),0);
 	  	recv(new.newSocket , fileInformation.file_content , sizeof(fileInformation.file_content) , 0);
-		send(new.newSocket,synch,sizeof(synch),0);
+		//send(new.newSocket,synch,sizeof(synch),0);
 	  	recv(new.newSocket , &fileInformation.mode , sizeof(fileInformation.mode) , 0);
-		send(new.newSocket,synch,sizeof(synch),0);
+		//send(new.newSocket,synch,sizeof(synch),0);
   		
 		  sprintf(copiedFilePath,"%s/%s",realPath,fileInformation.filename);
 		  //printf("file name: %s \n",fileInformation.filename);
 		  //printf("file Content: %s \n",fileInformation.file_content);
-		  printf("mode: %d \n",fileInformation.mode);
+		  //printf("mode: %d \n",fileInformation.mode);
 		  //recv(newSocket , &fileInformation.file_counter , sizeof(fileInformation.file_counter) , 0);
 	  
 		if(fileInformation.mode==0){
-			  printf("file name: %s \n",fileInformation.filename);
+			  //printf("file name: %s \n",fileInformation.filename);
 		  	  //printf("file Content: %s \n",fileInformation.file_content);
 		 	  // printf("file counter: %d \n",fileInformation.file_counter);
 		  	  sprintf(copiedFile,"%s/%s",new.serverName,fileInformation.filename);
@@ -235,10 +238,10 @@ void receive(mainSocket new,char *realPath){
 			  
 	  	}
 	  	else {
-			printf("directory name: %s \n ",copiedFilePath);
+			//printf("directory name: %s \n ",copiedFilePath);
 			directory=opendir(copiedFilePath);
 			if(directory==NULL){
-				printf("girdim2 \n");
+				//printf("girdim2 \n");
 				if(mkdir(copiedFilePath, S_IRUSR | S_IWUSR | S_IXUSR)==-1)
 					fprintf(stderr,"Directory can not create successfully \n");
 					receive(new,copiedFilePath);
